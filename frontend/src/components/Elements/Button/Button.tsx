9c78871848d433f00b4ce49d1c4a983f034cce77
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2023-09-16 11:31:13 +0900

    components

---
+++ frontend/src/components/Elements/Button/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    (
+        {
+            type = 'button',
+            ...props
+        },
+        ref
+    ) => {
+        return (
+            <button
+            style={
+                {
+                    backgroundColor: 'red',
+                    color: 'white'
+                }
+            }
+                ref={ref}
+                type={type}
+                {...props}
+            />
+        );
+    }
+);
