1f6720fb9240927e4106439e6a95a02055aa21f6
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2023-09-16 11:35:09 +0900

    prettier

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 
 // https://vitejs.dev/config/
@@ -10,4 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
